6102f4c5dd93d3723dbfa9d5520608490cea4cbd
tattyseal <iamtobys@gmail.com>
2013-11-27 19:32:29 +0000

    Added more music

---
+++ common/OpenXmasMod/common/ChristmasMusicTab.java
@@ -0,0 +1,19 @@
+package OpenXmasMod.common;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+
+public class ChristmasMusicTab extends CreativeTabs
+{
+	public ChristmasMusicTab(int id, String s)
+	{
+		super(id, s);
+		this.hasSearchBar();
+		setBackgroundImageName("item_search.png");
+	}
+	
+	public ItemStack getIconItemStack()
+	{
+		return new ItemStack(OpenXmasModCore.allIWantForXmasMusicDisk);
+	}
+}
